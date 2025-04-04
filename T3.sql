682628a0261c4cfc0ab1d97ad05d1a3411952a85
gabriel-leone <leonepgabriel@gmail.com>
2025-04-04 15:54:03 -0300

    feat: adding constructors, driver and countries tables

--- T3.sql
+++ T3.sql
@@ -9,4 +9,38 @@ CREATE TABLE circuits (
     alt int,
     url VARCHAR,
     PRIMARY KEY (circuit_id)
+);
+
+CREATE TABLE constructors (
+    constructor_id INT,
+    constructor_ref VARCHAR(100),
+    name VARCHAR(255),
+    nationality VARCHAR(100),
+    URL VARCHAR,
+    PRIMARY KEY (constructor_id)
+);
+
+CREATE TABLE driver (
+    driver_id INT,
+    driver_ref VARCHAR(100),
+    number INT, -- aqui talvez seja string
+    code VARCHAR(3),
+    forename VARCHAR(100),
+    surname VARCHAR(100),
+    date_of_birth DATE,
+    nationality VARCHAR(100),
+    URL VARCHAR,
+    PRIMARY KEY (driver_id)
+);
+
+CREATE TABLE countries (
+    country_id INT,
+    code VARCHAR(2),
+    name VARCHAR(255),
+    continent VARCHAR(2),
+    wikipedia_link VARCHAR,
+    keywords VARCHAR,
+    PRIMARY KEY (country_id)
+    -- adicionar uma possível constraint de continente (são poucos valores)
+    -- adicionar uma possível unicidade em code
 )
